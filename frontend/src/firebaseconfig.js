0c0bf99494efaf05da80095a7c4650881cc8179c
prem14-git <premkulkarni1407@gmail.com>
2025-07-31 01:20:45 +0530

    deployement

---
+++ frontend/src/firebaseconfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC5bA-YYaLh_hchAGgug7uqv_rcw2cmDPs",
+  authDomain: "assign-44566.firebaseapp.com",
+  projectId: "assign-44566",
+  storageBucket: "assign-44566.firebasestorage.app",
+  messagingSenderId: "180515872794",
+  appId: "1:180515872794:web:139c8743c3917b22b431a3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
